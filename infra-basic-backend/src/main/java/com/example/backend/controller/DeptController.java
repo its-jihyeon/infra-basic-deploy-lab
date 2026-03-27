59bde0455cfd463e69f6abddfb08be42c9459673
jihyeon <its.jihyeon@gmail.com>
2026-03-27 11:37:28 +0900

    update DeptController

--- infra-basic-backend/src/main/java/com/example/backend/controller/DeptController.java
+++ infra-basic-backend/src/main/java/com/example/backend/controller/DeptController.java
@@ -16,6 +16,11 @@ public class DeptController {
 
     private final DeptService deptService;
     
+    @GetMapping
+    public String getApi() {
+        return "backend-api";
+    }
+    
     @GetMapping("/depts")
     public List<Dept> getAll() {
         return deptService.findAll();
